d6a5a40b48dae7d761164b44f09abb213a8b284e
arieljhy <ariel_jhy@163.com>
2025-12-30 01:35:09 +0800

    fix: 修复 index.d.ts 类型引用错误

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,9 +2,9 @@ export { ChatSDK } from './core/ChatSDK'
 export { createChatSDK } from './index'
 
 export interface ChatSDKExports {
-  ChatSDK: typeof ChatSDK
-  createChatSDK: (config?: ChatSDKConfig) => ChatSDK
-  default: typeof ChatSDK
+  ChatSDK: typeof import('./core/ChatSDK').ChatSDK
+  createChatSDK: (config?: import('./core/ChatSDK').ChatSDKConfig) => import('./core/ChatSDK').ChatSDK
+  default: typeof import('./core/ChatSDK').ChatSDK
 }
 
 declare const sdk: ChatSDKExports
